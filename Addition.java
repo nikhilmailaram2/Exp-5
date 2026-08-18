71fbb8869f0830d8f2d7b236e07417f054abaa3c
nikhilmailaram2 <nikhilmailaram2@gmail.com>
2026-08-18 11:44:21 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("First number = " + a);
+        System.out.println("Second number = " + b);
+        System.out.println("Addition = " + sum);
+    }
+}public class Addition {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
